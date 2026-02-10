359d7e4835e399dd66124a45f87c955438a7db9a
sudhee krishna <sudheekrishna100@gmail.com>
2026-02-10 16:00:51 +0530

    login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -196,9 +196,10 @@ export const loginUser = async (req, res) => {
     // ✅ SET COOKIE
     res.cookie("token", token, {
       httpOnly: true,
-      // secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      // sameSite: "none",
+      // secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
