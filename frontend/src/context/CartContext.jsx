40e8beea60a0f42559d8a0dd6fd6c5c8af8c3e48
sudhee krishna <sudheekrishna100@gmail.com>
2026-02-27 14:39:02 +0530

    cart updation

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -9,51 +9,56 @@ export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
   const [openSideCart, setOpenSideCart] = useState(false);
   const [cartMeta, setCartMeta] = useState({
-  totalPrice: 0,
-  taxAmount: 0,
-  totalWithTax: 0
-});
+    totalPrice: 0,
+    taxAmount: 0,
+    totalWithTax: 0
+  });
   const { user } = useAuth();
 
 
   // 🔥 Fetch cart from backend
 
-const refreshCart = async () => {
-  if (!user) return;
-  try {
-    const res = await getCartApi();
-    setCartItems(res.data.items || []);
-    setCartMeta({
-      totalPrice: res.data.totalPrice || 0,
-      taxAmount: res.data.taxAmount || 0,
-      totalWithTax: res.data.totalWithTax || 0
-    });
-  } catch (err) {
-    if (err.response?.status !== 401) {
-      console.log("Refresh cart error", err.message);
+  const refreshCart = async () => {
+    if (!user) return;
+    try {
+      const res = await getCartApi();
+      // setCartItems(res.data.items || []);
+      const safeItems = (res.data.items || []).filter(
+        (item) => item && item.product
+      );
+
+      setCartItems(safeItems);
+      setCartMeta({
+        totalPrice: res.data.totalPrice || 0,
+        taxAmount: res.data.taxAmount || 0,
+        totalWithTax: res.data.totalWithTax || 0
+      });
+    } catch (err) {
+      if (err.response?.status !== 401) {
+        console.log("Refresh cart error", err.message);
+      }
     }
-  }
-};
+  };
 
 
 
 
   // 🔥 Auto load cart when user logs in or page refreshes
 
-useEffect(() => {
-  if (!user) {
-    setCartItems([]);
-    setOpenSideCart(false);   // 🔥 FORCE CLOSE SIDECART ON LOGOUT
-  } else {
-    refreshCart();
-  }
-}, [user]);
+  useEffect(() => {
+    if (!user) {
+      setCartItems([]);
+      setOpenSideCart(false);   // 🔥 FORCE CLOSE SIDECART ON LOGOUT
+    } else {
+      refreshCart();
+    }
+  }, [user]);
 
 
-useEffect(() => {
-  // console.log("USER:", user);
-  // console.log("CART:", cartItems);
-}, [user, cartItems]);
+  useEffect(() => {
+    // console.log("USER:", user);
+    // console.log("CART:", cartItems);
+  }, [user, cartItems]);
 
 
 
