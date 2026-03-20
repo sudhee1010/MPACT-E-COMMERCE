cdeffbbd8a1b9293a7c2d03c2001e30e06ef4b5f
sudhee krishna <sudheekrishna100@gmail.com>
2026-03-20 08:10:39 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -48,14 +48,12 @@ connectDB();
 app.set("trust proxy", 1);
 app.use(
   cors({
-    origin: "https://mpact-e-commerce.onrender.com",
+    // origin: "https://mpact-e-commerce.onrender.com",
     // origin: ["https://mpact-e-commerce.onrender.com",
     // "http://13.48.193.184:3000"],
     // origin: "http://localhost:3000",
     // origin:"https://mpact-e-commerce-1-0222.onrender.com",
-    // origin: [
-    //   "https://mpact-e-commerce-1-0222.onrender.com",
-    // ],
+    origin: ["https://mpact-e-commerce-1-0222.onrender.com","https://mpact-e-commerce.onrender.com" ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseurl:"",
-  // baseurl:"https://mpact-e-commerce-2-elbb.onrender.com/api",
+  // baseurl: "",
+  baseurl:"https://mpact-e-commerce-2-elbb.onrender.com",
   // baseURL: "http://13.48.193.184:5000",
   // baseURL: "http://localhost:5000",
   withCredentials: true,
